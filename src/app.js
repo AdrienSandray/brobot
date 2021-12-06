bf97ec939e29bc4201803104a814e609e2fbf915
AdrienSandray <71703884+AdrienSandray@users.noreply.github.com>
2021-12-06 22:20:17 +0100

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import tmi from 'tmi.js'
-import { BOT_USERNAME , OAUTH_TOKEN, OAUTH_TOKEN_BOT, CHANNEL_NAME} from './constants'
+import { BOT_USERNAME , OAUTH_TOKEN, OAUTH_TOKEN_BOT, CHANNEL_NAME, QUESTIONS, REPONSES} from './constants'
 
 const options = {
 	options: { debug: true },
@@ -14,11 +14,19 @@ const options = {
 		username: BOT_USERNAME,
 		password: OAUTH_TOKEN_BOT
 	},
-	channels: [ CHANNEL_NAME ]
+	channels: [CHANNEL_NAME]
 }
 
 const client = new tmi.Client(options)
+
+var question = 0
+var activated = 0
+var points = {}
+
 client.connect();
+
+
+///////////////////////////////EVENT HANDLERS /////////////////////////
 
 client.on('message', (channel, userstate, message, self) => {
 	if(self) {
@@ -45,6 +53,16 @@ client.on('message', (channel, userstate, message, self) => {
 		return
 	}
 
+	if(message.toLowerCase() === '!quizz') {
+		var display = quizzMsg()
+		client.say(channel, display)
+	}
+
+	if (message.toLowerCase() === REPONSES[question%3] && activated){
+		bonneReponse(channel, userstate)
+		return
+	}
+	
 	let msg = message.split(" ")
 	if(msg[0] === '!shot'){
 		if(msg[1] === undefined){client.say(channel, `@${userstate.username} faut mettre un blaze apres la commande.`)}
@@ -60,13 +78,14 @@ client.on('message', (channel, userstate, message, self) => {
 
 setInterval(updateTime, 60000);
 
-/*definitions des fonctions */
+///////////////////// DEFINITIONS DES FONCTIONS ///////////////////////
 
 function help (channel, userstate) {
 	client.say(channel, `@${userstate.username}, Liste des commandes : 
 	\n !disc
 	\n !yt
-	\n !shot`)
+	\n !shot
+	\n !quizz`)
 }
 
 function shot(channel, userstate, pseudo){
@@ -79,12 +98,36 @@ function shot(channel, userstate, pseudo){
 	}
 }
 
+
 function updateTime() {
     const date = new Date();
-    const minutes = date.getMinutes();
-	if (minutes % 15 === 0){
-		client.say(CHANNEL_NAME, 'oeeeeeeeeeeeee bien ?')
-	}
+	generateQuestion(date)
 }
 
 
+function generateQuestion(date) {
+    const minutes = date.getMinutes();
+	if (minutes % 15 === 0){
+		activated = 1
+		question = question + 1
+		client.say(CHANNEL_NAME, QUESTIONS[question%3])
+	}
+}
+
+function bonneReponse(channel, userstate) {
+	activated = 0
+	client.say(channel, `@${userstate.username} bonne réponse, +1 point`)
+	if (points[userstate.username]) {points[userstate.username] += 1}
+	else {points[userstate.username] = 1}
+}
+
+function quizzMsg(){
+	if(Object.keys(points).length === 0){return 'Pas encore de joueurs.'}
+	var display = 'Les points du quizz :'
+	for (var k in points){
+		var l = points[k]
+		display += ' ' + k + ' : ' + points[k] + ' |'
+	}
+	display += ' le reste : 0.'
+	return display
+}
--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,11 @@ export const CHANNEL_NAME = '#brocuplive';
 export const OAUTH_TOKEN = 'oauth:lw0t71a5ik7met9y74lazz0qebc30o';
 export const OAUTH_TOKEN_BOT = "oauth:5zjmwgtdtzgqup46t30kfr4t97uw6p";
 export const BOT_USERNAME = 'znoopie_le_glacon';
+
+export const QUESTIONS = [
+    'Qui est la vainquer de la BC s1 ? A - alex | B - Ivan | C - Etienne | D - Baptiste',
+    'Qui est la vainquer de la BC s2 ? A - alex | B - Ivan | C - Dylan | D - Baptiste',
+    'Qui est la vainquer de la BC s3 ? A - alex | B - Ivan | C - Macron | D - Baptiste' 
+]
+
+export const REPONSES = ['a', 'b', 'd']
