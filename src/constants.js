44d4000db31377b7e69b72da8e9e3b8a7c2cb537
AdrienSandray <71703884+AdrienSandray@users.noreply.github.com>
2021-12-06 22:41:02 +0100

    Add files via upload

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const CHANNEL_NAME = '#brocuplive';
-export const OAUTH_TOKEN = 'oauth:lw0t71a5ik7met9y74lazz0qebc30o';
-export const OAUTH_TOKEN_BOT = "oauth:5zjmwgtdtzgqup46t30kfr4t97uw6p";
+export const OAUTH_TOKEN = 'channel token';
+export const OAUTH_TOKEN_BOT = "put your bot token";
 export const BOT_USERNAME = 'znoopie_le_glacon';
 
 export const QUESTIONS = [
